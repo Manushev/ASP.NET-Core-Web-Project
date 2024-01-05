4777e0df3da2ad9274d442242f92daee7ebe1390
Yuri Ognyanov <manushevit@gmail.com>
2024-01-05 22:14:02 +0200

    Added ApplicationDbContext
    Also updated Connection string to connect with my DataBase. Create DataBase with name: Bulky

---
+++ BulkyWeb/Bulky/BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
