3fafd85425cc682bf086ed06a9ca743a005c62c5
Yuri Ognyanov <manushevit@gmail.com>
2024-01-07 00:04:48 +0200

    Get and Display all Categories

--- BulkyWeb/Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> listOfCategory = _context.Categories.ToList();
+            return View(listOfCategory);
         }
     }
 }
