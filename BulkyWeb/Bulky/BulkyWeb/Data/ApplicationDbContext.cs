49194e3bb6365ae9d566773e8fa606c3e3a700d7
Yuri Ognyanov <manushevit@gmail.com>
2024-01-06 22:33:34 +0200

    Added some data in Category table

--- BulkyWeb/Bulky/BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -10,7 +10,15 @@ namespace BulkyWeb.Data
         {
 
         }
-
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Action", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Action", DisplayOrder = 3 }
+            );
+        }
     }
 }
