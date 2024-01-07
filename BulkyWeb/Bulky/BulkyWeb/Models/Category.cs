a5445d0d5263de93db704beacd1dcf81dc52bda1
Yuri Ognyanov <manushevit@gmail.com>
2024-01-07 20:14:17 +0200

    Input Tag Helpers in Category model

--- BulkyWeb/Bulky/BulkyWeb/Models/Category.cs
+++ BulkyWeb/Bulky/BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -8,7 +9,10 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
