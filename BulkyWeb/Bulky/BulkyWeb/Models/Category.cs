7814cbfbb143fc806fd722069fedd1494c2519b1
Yuri Ognyanov <manushevit@gmail.com>
2024-01-13 23:58:26 +0200

    Changed to the ErrorMessage

--- BulkyWeb/Bulky/BulkyWeb/Models/Category.cs
+++ BulkyWeb/Bulky/BulkyWeb/Models/Category.cs
@@ -14,7 +14,7 @@ namespace BulkyWeb.Models
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
-        [Range(1, 100, ErrorMessage = "The field Display Order must be between 1-100")]
+        [Range(1, 100, ErrorMessage = "Please enter a display order between 1 and 100.")]
         public int DisplayOrder { get; set; }
     }
 }
