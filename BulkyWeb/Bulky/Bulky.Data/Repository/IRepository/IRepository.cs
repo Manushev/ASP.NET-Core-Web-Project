147e6e9945351f6a7f287ae15e61a4535363108f
Yuri Ognyanov <manushevit@gmail.com>
2024-01-16 23:57:45 +0200

    Added Repository folder in the Data project

---
+++ BulkyWeb/Bulky/Bulky.Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Data.Repository.IRepository
+{
+    internal interface IRepository
+    {
+    }
+}
